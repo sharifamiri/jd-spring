1d4c69c8e483e9cc16fac6162d44fe9d3979fa33
sharifamiri <sharif.amiri4@gmail.com>
2020-10-17 11:47:11 -0700

    Spring Container

---
+++ Spring-01-SpringContainer/src/main/java/CybertekAppp.java
@@ -0,0 +1,23 @@
+import interfaces.Mentor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import services.FullTimeMentor;
+
+public class CybertekAppp {
+    public static void main(String[] args) {
+
+        //1st Way
+        //BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+
+        //2nd Way;
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        //Mentor mentor = new FullTimeMentor();
+        //Mentor mentor = (Mentor)container.getBean("partTimeMentor");
+
+        Mentor mentor = container.getBean("fullTimeMentor",Mentor.class);
+
+        mentor.createAccount();
+    }
+}
