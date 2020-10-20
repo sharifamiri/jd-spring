68ed3eebdf6b56cc775dff1898a467b82be5f730
sharifamiri <sharif.amiri4@gmail.com>
2020-10-20 16:57:30 -0700

    Constructor Injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,13 +1,24 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
 
-    OfficeHours officeHours;
+//    OfficeHours officeHours;
+
+    //We decide in xml file which one to call
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+    //    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
+        System.out.println("Weekly teaching hours : " + (20 + extraSessions.getHours()));
     }
 }
