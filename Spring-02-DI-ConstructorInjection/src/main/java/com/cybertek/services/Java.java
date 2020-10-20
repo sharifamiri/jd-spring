6a6c74cc7dc475e20f0b5a2eece94cea881f98d5
sharifamiri <sharif.amiri4@gmail.com>
2020-10-20 16:27:07 -0700

    Constructor Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,13 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+public class Java implements Course {
+
+    OfficeHours officeHours;
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
+    }
+}
