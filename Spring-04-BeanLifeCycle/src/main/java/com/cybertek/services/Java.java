43fa461437c543a74379a1360da0494da58e0af4
sharifamiri <sharif.amiri4@gmail.com>
2020-10-20 18:21:45 -0700

    bean life cycle

---
+++ Spring-04-BeanLifeCycle/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,17 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+public class Java implements Course {
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching hours 20");
+    }
+
+    public void myInitMethod(){
+        System.out.println("Executing init method");
+    }
+
+    public void myDestroyMethod(){
+        System.out.println("Executing destroy method");
+    }
+}
