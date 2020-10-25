8999a1ad36b338d738c2897e0fbb2a31ed00b020
sharifamiri <sharif.amiri4@gmail.com>
2020-10-25 13:56:32 -0700

    java code - configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/CybertekApp.java
@@ -14,5 +14,7 @@ public class CybertekApp {
         Course course = container.getBean("java",Course.class);
 
         course.getTeachingHours();
+
+        System.out.println(course.toString());
     }
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -1,10 +1,38 @@
 package com.cybertek.configs;
 
+import com.cybertek.interfaces.ExtraSessions;
+import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
+import com.cybertek.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.cybertek")
+@PropertySource("classpath:application.properties")
 public class CybertekAppConfig {
 
+    @Bean
+    public Java java(){
+        return new Java(extraSessions());
+    }
+
+    @Bean
+    public Selenium selenium(){
+        return new Selenium();
+    }
+
+    /*
+    @Bean
+    public OfficeHours officeHours(){
+        return new OfficeHours();
+    }
+     */
+
+    @Bean  //@Bean is optional bec it is a parameter for "java"
+    public ExtraSessions extraSessions(){ //ExtraSessions ex = new Office Hours();
+        return new OfficeHours();
+    }
 }
