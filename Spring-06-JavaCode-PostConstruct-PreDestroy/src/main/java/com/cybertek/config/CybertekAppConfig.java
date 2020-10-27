8e4f23226da19e2ae91f00453b59c42b2ba4f289
sharifamiri <sharif.amiri4@gmail.com>
2020-10-27 16:26:24 -0700

    java code - post-construct and pre-destroy

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/config/CybertekAppConfig.java
@@ -0,0 +1,17 @@
+package com.cybertek.config;
+
+import com.cybertek.interfaces.ExtraSessions;
+import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
+import com.cybertek.services.Selenium;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.cybertek")
+public class CybertekAppConfig {
+
+
+}
