8000a7f3725ed16e4f9c9a9327140db890fffb17
sharifamiri <sharif.amiri4@gmail.com>
2020-10-27 22:10:41 -0700

    spring boot - demo

--- Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/services/Java.java
+++ Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/services/Java.java
@@ -1,14 +1,25 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    @Value("${instructor}")
+    private String instructorName;
+
+    ExtraSession extraSession;
+
+    public Java(ExtraSession extraSession) {
+        this.extraSession = extraSession;
+    }
 
     @Override
     public int getTeachingHours() {
-        return 20;
+        System.out.println(instructorName);
+        return 20 + extraSession.getHours();
     }
 }
