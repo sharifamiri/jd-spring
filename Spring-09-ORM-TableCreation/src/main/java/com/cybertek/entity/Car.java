cf54e93271af240c601c377a38d7bb4ef333ee26
sharifamiri <sharif.amiri4@gmail.com>
2020-12-12 11:14:57 -0800

    ORM Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,12 +1,13 @@
 package com.cybertek.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "cars")
 public class Car {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String make;
---
+++ Spring-09-ORM-TableCreation/src/main/resources/data2.sql
@@ -0,0 +1,4 @@
+insert into students(first_name,last_name,email) values ('Mike','Smith','mike@cybertek.com');
+insert into students(first_name,last_name,email) values ('Adam','Jons','adam@cybertek.com');
+insert into students(first_name,last_name,email) values ('Tom','Tally','tom@cybertek.com');
+insert into students(first_name,last_name,email) values ('Brady','Stone','brad@cybertek.com');
