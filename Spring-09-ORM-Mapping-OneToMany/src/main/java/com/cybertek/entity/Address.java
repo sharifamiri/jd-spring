dda11352cdb292407857a7774e7803ad04e1586e
sharifamiri <sharif.amiri4@gmail.com>
2020-12-16 17:23:46 -0800

    ORM-Mapping-OneToMany

--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
@@ -1,11 +1,22 @@
 package com.cybertek.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Address {
 
+    public Address(String street, String zipCode) {
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String street;
--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -1,11 +1,21 @@
 package com.cybertek.entity;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.*;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Person {
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,7 +27,7 @@ public class Person {
     @OneToMany(mappedBy = "person")
     private List<Address> addresses;
 
-//        @OneToMany
+//        @OneToMany(cascade = CascadeType.ALL)
 //        @JoinColumn(name = "person_id")
 //        private List<Address> addresses;
 }
