b70a19eb9db53486c0d16b64ff268818dd0d0777
sharifamiri <sharif.amiri4@gmail.com>
2020-12-23 16:26:12 -0800

    ORM-JPQL-NamedQueries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -22,5 +23,13 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
    //multiple bind parameter
    @Query("select  e from  Employee e where e.email = ?1 and e.salary = ?2")
    Employee getEmployeeByEmailAndSalary(String email, int salary);
+
+   //single named parameter
+   @Query("select e from Employee e where e.salary=:salary")
+   Employee getEmployeeBySalary(@Param("salary") int salary);
+
+   //multiple named parameters
+    @Query("select e from Employee e where e.firstName=:name or e.salary=:salary")
+    List<Employee> getEmployeeByFirstNameOrSalary(@Param("name") String name, @Param("salary") int salary);
 
 }
