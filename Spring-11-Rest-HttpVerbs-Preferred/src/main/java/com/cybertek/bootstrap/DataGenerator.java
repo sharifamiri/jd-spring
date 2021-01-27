149c7bf7151ee41d812b88fcfe29bbc5c3c73b1a
sharifamiri <sharif.amiri4@gmail.com>
2021-01-26 16:20:01 -0800

    Spring REST - HttpVerbs Preferred

---
+++ Spring-11-Rest-HttpVerbs-Preferred/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,30 @@
+package com.cybertek.bootstrap;
+
+import com.cybertek.entity.Product;
+import com.cybertek.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    private ProductRepository productRepository;
+
+    public DataGenerator(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Product pc = new Product("DELL");
+        Product laptop = new Product("McBook");
+        Product phone = new Product("iPhone");
+        Product tablet = new Product("iPad");
+
+        productRepository.save(pc);
+        productRepository.save(laptop);
+        productRepository.save(phone);
+        productRepository.save(tablet);
+
+    }
+}
