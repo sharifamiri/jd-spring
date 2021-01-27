329bb9941617240bbf1b3fb9f494d4dc2511f030
sharifamiri <sharif.amiri4@gmail.com>
2021-01-26 17:50:44 -0800

    Spring REST - Jackson

---
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/MovieRepository.java
@@ -0,0 +1,56 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+    // ------------------- DERIVED QUERIES ------------------- //
+//Write a derived query to read a movie with a name
+    Optional<Movie> findByName(String name);
+
+//Write a derived query to list all movies between a range of prices
+    List<Movie> findAllByPriceBetween(BigDecimal price1, BigDecimal price2);
+
+//Write a derived query to list all movies where duration exists in the specific list of duration
+    List<Movie> findAllByDurationIn(List<Integer> durations);
+
+//Write a derived query to list all movies with higher than a specific release date
+    List<Movie> findAllByReleaseDateAfter(LocalDate releaseDate);
+
+//Write a derived query to list all movies with a specific state and type
+    List<Movie> findAllByStateAndType(String state, String type);
+
+// ------------------- JPQL QUERIES ------------------- //
+//Write a JPQL query to list all movies between a range of prices
+    @Query("select m from Movie m where m.price between ?1 and ?2")
+    List<Movie> fetchAllBetweenPriceRange(BigDecimal price1, BigDecimal price2);
+
+//Write a JPQL query that returns all movie names
+    @Query("select distinct m.name from Movie m")
+    List<String> fetchAllMovieNames();
+
+// ------------------- Native QUERIES ------------------- //
+//Write a native query that returns a movie by name
+    @Query(value = "select * from movie where name=?1",nativeQuery = true)
+    Optional<Movie> retrieveByName(String name);
+
+//Write a native query that return the list of movies in a specific range of prices
+    @Query(value = "select * from movie where price between ?1 and ?2",nativeQuery = true)
+    List<Movie> retrieveByPriceRange(BigDecimal price1, BigDecimal price2);
+
+//Write a native query to return all movies where duration exists in the range of duration
+    @Query(value = "select * from movie where duration in ?1",nativeQuery = true)
+    List<Movie> retrieveByDurationInRange(List<Integer> durations);
+
+//Write a native query to list the top 5 most expensive movies
+    @Query(value = "select * from movie order by price desc limit 5",nativeQuery = true)
+    List<Movie> top5ExpensiveMovie();
+}
---
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/TicketRepository.java
@@ -0,0 +1,69 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
+
+    // ------------------- DERIVED QUERIES ------------------- //
+
+    //Write a derived query to count how many tickets a user bought
+    Integer countAllByUserId(Long userId);
+//    Integer countTicketByUserId(Long userId);
+
+    //Write a derived query to list all tickets by specific email
+    List<Ticket> findAllByUserEmail(String email);
+
+    //Write a derived query to count how many tickets are sold for a specific movie
+    Integer countAllByMovieCinemaMovieName(String name);
+
+    //Write a derived query to list all tickets between a range of dates
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime date1, LocalDateTime date2);
+
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that returns all tickets are bought from a specific user
+    @Query("select t from Ticket t where t.user.id=?1")
+    List<Ticket> fetchAllTicketsByUserJPQL(Long userId);
+
+    //Write a JPQL query that returns all tickets between a range of dates
+    @Query("select t from Ticket t where t.dateTime between ?1 and ?2")
+    List<Ticket> retrieveAllTicketsByRangeDate(LocalDateTime date1, LocalDateTime date2);
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query to count the number of tickets a user bought
+    @Query(value = "select count(*) from ticket where user_account_id=?1",nativeQuery = true)
+    Integer countTicketByUser(Long userId);
+
+    //Write a native query to count the number of tickets a user bought in a specific range of dates
+    @Query(value = "select count(*) from ticket where user_account_id=?1 and date_time between ?2 and ?3",nativeQuery = true)
+    Integer countTicketsByUserInDateRanges(Long userId,LocalDateTime start, LocalDateTime end);
+
+    //Write a native query to distinct all tickets by movie name
+    @Query(value = "select distinct(m.name) from ticket t join movie_cinema mc on mc.id=t.movie_cinema_id join movie m on mc.movie_id=m.id)",nativeQuery = true)
+    List<String> retrieveAllDistinctMovieName();
+
+    //Write a native query to find all tickets by user email
+    @Query(value = "select * from ticket t join user_account ua on t.user_account_id=ua.id where ua.email=?1",nativeQuery = true)
+    List<Ticket> retrieveAllByUserEmail(String email);
+
+    //Write a native query that returns all tickets
+    @Query(value = "select * from ticket",nativeQuery = true)
+    List<Ticket> retrieveAll();
+
+    //Write a native query to list all tickets where a specific value should be containable in the username or name or movie name
+    @Query(value = "select * from ticket t join user_account ua on t.user_account_id=ua.id" +
+            " join account_details ad on ad.id=ua.account_details_id" +
+            " join movie_cinema mc on t.movie_cinema_id=mc.id" +
+            " join movie m on mc.movie_id = m.id" +
+            " where ua.username ilike concat('%',?1,'%') or ad.name ilike concat('%',?1,'%') or m.name ilike concat('%',?1,'%')",nativeQuery = true)
+    List<Ticket> retrieveAllBySearchCriteria(String searchCriteria);
+
+}
