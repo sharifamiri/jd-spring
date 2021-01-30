51a8ecd4223a76b1c8541d0543a5c95f904c3d3a
sharifamiri <sharif.amiri4@gmail.com>
2021-01-30 09:22:16 -0800

    Spring REST - School Lab

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Address.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Address.java
@@ -47,9 +47,9 @@ public class Address extends BaseEntity {
 
     private Integer currentTemperature;
 
-    private Integer getCurrentTemperature(){
-        return consumeTemp(this.city);
-    }
+//    private Integer getCurrentTemperature(){
+//        return consumeTemp(this.city);
+//    }
 
     public Integer consumeTemp(String city){
 
