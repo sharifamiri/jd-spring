d7e59a256170c566d51d4e875892e1eca9d633af
sharifamiri <sharif.amiri4@gmail.com>
2021-01-30 07:39:11 -0800

    Spring REST - School Lab

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,9 +1,44 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.ResponseWrapper;
+import com.cybertek.model.Teacher;
+import com.cybertek.repository.ParentRepository;
+import com.cybertek.repository.StudentRepository;
+import com.cybertek.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApiController {
 
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private ParentRepository parentRepository;
 
+    @GetMapping("/teachers")
+    public List<Teacher> readAllTeachers(){
+        return teacherRepository.findAll();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
+        return ResponseEntity
+                .ok(new ResponseWrapper("students are successfully retrieved",
+                        studentRepository.findAll()));
+    }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are successfully retrieved",
+                HttpStatus.ACCEPTED.value(), parentRepository.findAll());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
+    }
 }
---
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/repository/ParentRepository.java
@@ -0,0 +1,7 @@
+package com.cybertek.repository;
+
+import com.cybertek.model.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentRepository extends JpaRepository<Parent, Long> {
+}
