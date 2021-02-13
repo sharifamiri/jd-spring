ea67657756dd0bc7a89833ff4c3cb74b8bf57493
sharifamiri <sharif.amiri4@gmail.com>
2021-02-13 12:24:17 -0800

    Spring Security - JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
@@ -8,6 +8,8 @@ import com.cybertek.exception.ServiceException;
 import com.cybertek.service.UserService;
 import com.cybertek.util.JWTUtil;
 import io.jsonwebtoken.Jwt;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Authenticate controller",description = "Authenticate API")
 public class AuthenticationController {
 
     @Autowired
@@ -28,6 +31,7 @@ public class AuthenticationController {
 
     @PostMapping("/authenticate")
     @DefaultExceptionMessage(defaultMessage = "Bad Credentials")
+    @Operation(summary = "Login to application")
     public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
         String password = authenticationRequest.getPassword();
         String username = authenticationRequest.getUsername();
@@ -42,6 +46,7 @@ public class AuthenticationController {
 
     @PostMapping("/create-user")
     @DefaultExceptionMessage(defaultMessage = "Failed to create user, please try again")
+    @Operation(summary = "Create a new user")
     public ResponseEntity<ResponseWrapper> createWrapper(@RequestBody User user) throws ServiceException {
         User createdUser = userService.createUser(user);
         return ResponseEntity.ok(new ResponseWrapper("User has been created successfully",createdUser));
